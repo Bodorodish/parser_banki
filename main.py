fac9712e3944955a655b077b035bcc4daf92fd3e
Никита <ptichatv@gmail.com>
2025-02-24 16:06:20 +0300

    adding main file

---
+++ main.py
@@ -0,0 +1,22 @@
+import schedule
+import time
+from parser import parse_reviews
+from statistic import ReviewAnalyzer
+
+def job():
+    print("Начинаем парсинг отзывов...")
+    parse_reviews()
+
+    print("Начинаем анализ отзывов...")
+    analyzer = ReviewAnalyzer('Отзывы.xlsx')
+    analyzer.process_reviews()
+
+    print("Задача завершена!")
+
+schedule.every(1).hour.do(job)
+
+if __name__ == "__main__":
+    print("Система готова к работе. Задачи будут выполняться через каждый час.")
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
